c902ae33f815a26cb7352dca0b6c38a5a2fb1494
Sutu Sebastian <sebiitv@gmail.com>
2025-07-22 15:09:08 +0300

    up packages

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ThemeInit } from "../../.flowbite-react/init";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,6 +9,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
+      <ThemeInit />
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
     </>
